01bc47b43761637acba84a7ea8cf6f495eb34884
Ricardo Fabio <ricardofabio@gmail.com>
2025-10-25 15:53:19 -0300

    Se actualiza para el soporte con SQLAchemy, creación de modelos User y Todo, utilización del shell de Flask, ejemplos de distintas consultas a la base de datos para insertar y obtener información

--- todolistproject/todor/__init__.py
+++ todolistproject/todor/__init__.py
@@ -1,22 +1,32 @@
 from flask import Flask, render_template
-from . import todo
-from . import auth
+from flask_sqlalchemy import SQLAlchemy
+
+# creamos la extensión
+db = SQLAlchemy()
 
 def create_app():
     app = Flask(__name__)
 
     app.config.from_mapping(
         DEBUG = True,
-        SECRET_KEY = 'dev'
+        SECRET_KEY = 'dev',
+        SQLALCHEMY_DATABASE_URI = 'sqlite:///todolist.db'
     )
-
+    #inicializamos la extensión 
+    db.init_app(app)
+    
     # Registrar el blueprint
+    from . import todo
     app.register_blueprint(todo.bp)
 
+    from . import auth
     app.register_blueprint(auth.bp)
     
     @app.route("/")
     def index():
         return render_template('index.html')
     
+    with app.app_context():
+        db.create_all()
+
     return app
