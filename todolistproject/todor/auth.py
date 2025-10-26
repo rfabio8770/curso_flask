217bfd7c3f8ad2687369aceeb75b57dfa86ea13b
Ricardo Fabio <ricardofabio@gmail.com>
2025-10-26 12:01:07 -0300

    Aplicación ToDoList terminada con ejemplo de CRUD para las tareas verificación de sesion, mantenimiento de sesión

--- todolistproject/todor/auth.py
+++ todolistproject/todor/auth.py
@@ -1,5 +1,5 @@
 from flask import (
-    Blueprint, render_template, request, url_for, redirect, flash
+    Blueprint, render_template, request, url_for, redirect, flash, session, g
 )
 
 from werkzeug.security import generate_password_hash, check_password_hash
@@ -32,6 +32,56 @@ def register():
         
     return render_template('auth/register.html')
 
-@bp.route('/login')
+@bp.route('/login', methods= ('GET', 'POST'))
 def login():
+    if request.method == 'POST':
+        username = request.form['username']
+        password = request.form['password']
+        
+       
+
+        error =  None
+        # validar los datos 
+
+        user = User.query.filter_by(username = username).first()
+        if user == None:
+            error = "Nombre de usuario incorrecto"
+        elif not check_password_hash(user.password, password):
+            error = "Contraseña Incorrecta"
+        
+        # iniciar sesión
+        if error is None:
+            session.clear()
+            session['user_id'] = user.id
+
+            return redirect(url_for('todo.index'))
+        
+        flash(error)
     return render_template('auth/login.html')
+
+# funcion para cargar el usuario que se logueó
+@bp.before_app_request  # hacemos que esta función se ejecute en cada petición
+def load_login_in_user():
+    user_id = session.get('user_id')
+
+    if user_id is None: # nadie inició sesión
+        g.user = None
+    else:
+        g.user = User.query.get_or_404(user_id)
+
+@bp.route('/logout')
+def logout(): # funcion para cerrar sesión'
+    session.clear()
+    return redirect(url_for('index'))
+
+
+import functools
+
+def login_required(view):
+    @functools.wraps(view)
+    def wrapped_view(**kwargs):
+        # un usuario inicio sesión
+        if g.user is None:
+            return redirect(url_for('auth.login'))
+        return view(**kwargs)
+    return wrapped_view
--- todolistproject/todor/todo.py
+++ todolistproject/todor/todo.py
@@ -1,7 +1,58 @@
-from flask import Blueprint
+from flask import Blueprint, render_template, request, redirect, url_for, g
+
+from todor.auth import login_required
+from .models import Todo, User
+from todor import db
 
 bp = Blueprint('todo', __name__, url_prefix='/todo')
 
 @bp.route('/list')
+@login_required
 def index():
-    return "Lista de Tareas"
+    todos = Todo.query.all()
+    return render_template('todo/index.html', todos = todos)
+
+@bp.route('/create', methods = ('GET','POST'))
+@login_required
+def crear_tarea():
+    if request.method == 'POST':
+        title = request.form['title']
+        desc = request.form['desc']
+        # creamos el objeto todo
+        todo = Todo(g.user.id, title, desc)
+        # guardamos el objeto todo  a la base de datos
+        db.session.add(todo)
+        db.session.commit()
+
+        return redirect(url_for('todo.index'))
+
+    return render_template('todo/create.html')
+
+
+def get_todo(id):
+    todo = Todo.query.get_or_404(id)
+    return todo
+
+@bp.route('/update/<int:id>', methods = ('GET','POST'))
+@login_required
+def update(id):
+    todo = get_todo(id)
+    if request.method == 'POST':
+        # modificamos los datos
+        todo.title = request.form['title']
+        todo.desc = request.form['desc']
+        todo.state = True if request.form.get('state') == 'on' else False
+    
+        # guardamos las modificaciones en la base de datos
+        db.session.commit()
+
+        return redirect(url_for('todo.index'))
+    return render_template('todo/update.html', todo = todo)
+
+@bp.route('/delete/<int:id>')
+@login_required
+def delete(id):
+    todo = get_todo(id)
+    db.session.delete(todo)
+    db.session.commit()
+    return redirect(url_for('todo.index'))
